358a8fa9649ca4d654d6a514820c9c32e1bbbb74
Espen Albert <espen.albert@mongodb.com>
2026-01-23 11:35:30 +0000

    chore: fix variable missing

--- examples/encryption/variables.tf
+++ examples/encryption/variables.tf
@@ -2,3 +2,9 @@ variable "project_id" {
   type        = string
   description = "MongoDB Atlas project ID"
 }
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = ""
+}
