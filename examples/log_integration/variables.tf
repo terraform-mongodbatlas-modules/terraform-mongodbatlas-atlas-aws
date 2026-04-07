ac54621d63108951e51c1f187b6e8bd26116fcc5
Espen Albert <espen.albert@mongodb.com>
2026-04-07 06:36:56 +0100

    test: log_integration example

---
+++ examples/log_integration/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  type        = string
+  description = "MongoDB Atlas project ID"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+  default     = null
+}
+
+variable "bucket_name_prefix" {
+  type        = string
+  default     = null
+  description = "S3 bucket name prefix for log integration. If null, uses auto-generated prefix based on project ID. Mutually exclusive with bucket_name."
+}
+
+variable "force_destroy" {
+  type        = bool
+  default     = false
+  description = "Allow bucket deletion even if not empty"
+}
+
+variable "aws_tags" {
+  type        = map(string)
+  description = "Tags to apply to AWS resources"
+  default     = {}
+}
