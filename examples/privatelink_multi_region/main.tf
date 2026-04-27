0221c587495c2eafdf2474304e4f015dbf3edc8c
Espen Albert <espen.albert@mongodb.com>
2026-04-27 10:38:55 +0100

    apply PR suggestions

--- examples/privatelink_multi_region/main.tf
+++ examples/privatelink_multi_region/main.tf
@@ -2,7 +2,7 @@ module "atlas_aws" {
   source     = "../../"
   project_id = var.project_id
 
-  privatelink_regional_mode = "auto" # "auto" enables regional mode when the module sees multiple distinct Atlas regions. Ensure you understand the tradeoffs before enabling.
+  privatelink_regional_mode = "auto" # enables regional mode when the module sees multiple distinct Atlas regions. Ensure you understand the tradeoffs before enabling.
 
   privatelink_endpoints = [
     { region = "us-east-1", subnet_ids = var.subnet_ids_us_east_1 },
